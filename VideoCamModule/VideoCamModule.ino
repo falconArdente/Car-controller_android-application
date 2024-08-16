d1546930914ffb1b65434edefaf302b787e5a3c7
falconArdente <iwsokolov@ya.ru>
2024-08-16 23:10:14 +0300

    Forming variables of class

--- VideoCamModule/VideoCamModule.ino
+++ VideoCamModule/VideoCamModule.ino
@@ -10,6 +10,24 @@ public:
     CameraStates cameraState = CAMS_OFF;
 
 private:
+    //timings
+    const uint16_t BOUNCE_DELAY = 60;
+    const uint16_t REPEATER_DELAY = 600; // millis to discover command to turn front camera on
+    const uint16_t FRONT_CAM_SHOWTIME_DELAY = 3000; //millis to show front cam after signal gone off
+    //input pins
+    const int inReverseGear = A1;
+    const int inRightTurn = 12;
+    const int inLeftTurn = A0;
+    //output pins
+    const int outRearCamPower = 7; // transistor to power on
+    const int outFrontCamPower = 5;
+    const int outCautionSignal = 2;
+    const int outDisplayOn = A2;
+    const int outLeftFogLight = 3;
+    const int outRightFogLight = 9;
+    const int outRelayCameraSwitch = 11;
+    const int outControllerLed = 13; //direct board on
+    // states
     int stateRearSig = 0;
     int stateLeftSig = 0;
     int stateRightSig = 0;
@@ -21,25 +39,23 @@ private:
     unsigned long lastTimeRightChanged = 0;
     unsigned long leftTriggerOffMill = 0;
     unsigned long rightTriggerOffMill = 0;
-};
 
-//input pins
-const int inReverseGear = A1;
-const int inRightTurn = 12;
-const int inLeftTurn = A0;
-//output pins
-const int outRearCamPower = 7; // transistor to power on
-const int outFrontCamPower = 5;
-const int outCautionSignal = 2;
-const int outDisplayOn = A2;
-const int outLeftFogLight = 3;
-const int outRightFogLight = 9;
-const int outRelayCameraSwitch = 11;
-const int outControllerLed = 13; //direct board on
-//timings
-const uint16_t BOUNCE_DELAY = 60;
-const uint16_t REPEATER_DELAY = 600; // millis to discover command to turn front camera on
-const uint16_t FRONT_CAM_SHOWTIME_DELAY = 3000; //millis to show front cam after signal gone off
+    bool checkSignalState(
+            int *lastSignalState,
+            const int pinNumber,
+            unsigned long *lastTimeChanged) {
+        if (lastSignalState != digitalRead(pinNumber) &&
+            lastTimeChanged <= millis() - BOUNCE_DELAY) {
+            lastSignalState = digitalRead(pinNumber);
+            lastTimeChanged = millis();
+            if (lastSignalState == HIGH) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+};
 
 
 void setup() {
