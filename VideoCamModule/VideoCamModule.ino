485e6f310201a62620c38328930ef346b6712fd4
falconArdente <iwsokolov@ya.ru>
2024-08-16 14:54:10 +0300

    Turning idea into a OOP pattern

--- VideoCamModule/VideoCamModule.ino
+++ VideoCamModule/VideoCamModule.ino
@@ -1,5 +1,28 @@
 #include <EEPROM.h>
 
+class CameraLightTurnsSupplyController {
+    enum CameraStates {
+        CAMS_OFF,
+        REAR_CAM_ON,
+        FRONT_CAM_ON,
+    };
+public:
+    CameraStates cameraState = CAMS_OFF;
+
+private:
+    int stateRearSig = 0;
+    int stateLeftSig = 0;
+    int stateRightSig = 0;
+    bool backIsOn = false;
+    bool leftIsOn = false;
+    bool rightIsOn = false;
+    unsigned long lastTimeRearChanged = 0;
+    unsigned long lastTimeLeftChanged = 0;
+    unsigned long lastTimeRightChanged = 0;
+    unsigned long leftTriggerOffMill = 0;
+    unsigned long rightTriggerOffMill = 0;
+};
+
 //input pins
 const int inReverseGear = A1;
 const int inRightTurn = 12;
@@ -17,19 +40,7 @@ const int outControllerLed = 13; //direct board on
 const uint16_t BOUNCE_DELAY = 60;
 const uint16_t REPEATER_DELAY = 600; // millis to discover command to turn front camera on
 const uint16_t FRONT_CAM_SHOWTIME_DELAY = 3000; //millis to show front cam after signal gone off
-// state variables
-int deviceState = 0;//0-all cams are off; 1-rear is ON front OFF; 2-front is ON rear is OFF
-int stateRearSig = 0;
-int stateLeftSig = 0;
-int stateRightSig = 0;
-bool backIsOn = false;
-bool leftIsOn = false;
-bool rightIsOn = false;
-unsigned long lastTimeRearChanged = 0;
-unsigned long lastTimeLeftChanged = 0;
-unsigned long lastTimeRightChanged = 0;
-unsigned long leftTriggerOffMill = 0;
-unsigned long rightTriggerOffMill = 0;
+
 
 void setup() {
     Serial.begin(9600);
