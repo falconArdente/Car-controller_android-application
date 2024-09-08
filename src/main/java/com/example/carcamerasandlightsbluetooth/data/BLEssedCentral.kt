7a2f22900e57845875c59b4b110d627e656e1044
falconArdente <iwsokolov@ya.ru>
2024-09-08 22:37:18 +0300

    add module for injections

---
+++ src/main/java/com/example/carcamerasandlightsbluetooth/data/BLEssedCentral.kt
@@ -0,0 +1,4 @@
+package com.example.carcamerasandlightsbluetooth.data
+
+class BLEssedCentral {
+}
---
+++ src/main/java/com/example/carcamerasandlightsbluetooth/di/Bluetooth.kt
@@ -0,0 +1,7 @@
+package com.example.carcamerasandlightsbluetooth.di
+
+import org.koin.dsl.module
+
+val bluetoothModule = module {
+
+}
