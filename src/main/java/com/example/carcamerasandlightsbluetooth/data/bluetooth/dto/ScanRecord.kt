8c19956a1dd2ed023ce765ce4c29ce261e33975b
falconArdente <iwsokolov@ya.ru>
2024-09-10 21:30:25 +0300

    did add scanning procedure with Flow support

--- src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/dto/ScanRecord.kt
+++
@@ -1,8 +0,0 @@
-package com.example.carcamerasandlightsbluetooth.data.bluetooth.dto
-
-data class ScanRecord(
-    val advertiseFlag: Int,
-    val mServiceUuids: List<String>,
-    val powerLevel: Int,
-    val deviceName: String,
-)
