14e419e6b58519c535e15f60197e5fd026a80c67
falconArdente <iwsokolov@ya.ru>
2024-09-13 23:51:34 +0300

    service discovery integration

--- src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/BLEssedCentral.kt
+++ src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/BLEssedCentral.kt
@@ -1,20 +1,27 @@
 package com.example.carcamerasandlightsbluetooth.data.bluetooth
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 
-class BLEssedCentral {
-    private var scanCallback: ScanCallback?=null
+class BLEssedCentral(private val context: Activity, private val scope: CoroutineScope) {
+    private var scanCallback: ScanCallback? = null
     private var adapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
     private var scanner: BluetoothLeScanner = adapter.bluetoothLeScanner
     private val scanSettings = ScanSettings.Builder()
@@ -45,6 +52,7 @@ class BLEssedCentral {
                 Log.d("BLEssedScan", "fail")
             }
         }
+
         scanningJob = launch {
             Log.d("BEL", "launchScan")
             scanner.startScan(null, scanSettings, scanCallback)
@@ -119,8 +127,37 @@ class BLEssedCentral {
             .setDeviceAddress(deviceAddress)
             .build()
     }
-    fun stopScan(){
+
+    fun stopScan() {
+        Log.d("BEL", "stopScan")
         scanningJob?.cancel()
-        scanner.stopScan(scanCallback)
+        scanner.stopScan(object : ScanCallback() {})
+    }
+
+    private val connectionStateCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.d("BEL", "connected to ${gatt.device}")
+                scope.launch {
+                    Log.d("BEL", "starting searching descovery")
+                    gatt.discoverServices()
+                    delay(1200L)
+                    gatt.services.forEach {
+                        Log.d("BEL", "service ${it.uuid}")
+                    }
+
+                }
+
+            } else {
+                gatt.close()
+            }
+        }
+    }
+
+    suspend fun connectTo(device: BluetoothDevice) {
+        Log.d("BEL", "connecting to ${device.name}")
+
+        val gatt: BluetoothGatt =
+            device.connectGatt(context, false, connectionStateCallback)
     }
 }
