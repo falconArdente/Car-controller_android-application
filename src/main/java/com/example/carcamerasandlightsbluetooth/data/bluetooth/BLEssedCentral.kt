50970dc33457222c6e7ed6278a669cdbff6c5713
falconArdente <iwsokolov@ya.ru>
2024-09-15 21:06:18 +0300

    done working data-layer. Lot of deprecated functions

--- src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/BLEssedCentral.kt
+++ src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/BLEssedCentral.kt
@@ -11,6 +11,7 @@ import android.bluetooth.BluetoothGatt.GATT_FAILURE
 import android.bluetooth.BluetoothGatt.GATT_SUCCESS
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
 import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
@@ -21,12 +22,17 @@ import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import com.welie.blessed.supportsNotify
+import com.welie.blessed.supportsNotifyOrIndicate
+import com.welie.blessed.supportsReading
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
 
 
 class BLEssedCentral(private val context: Activity, private val scope: CoroutineScope) {
@@ -134,26 +140,93 @@ class BLEssedCentral(private val context: Activity, private val scope: Coroutine
 
     fun stopScan() {
         scanJob?.let { scanJob!!.cancel() }
+
     }
 
     private val connectionStateCallback = object : BluetoothGattCallback() {
         override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
             super.onServicesDiscovered(gatt, status)
             if (status == GATT_FAILURE) {
-                Log.d("BEL", "Service discovery failed");
+                Log.d("BEL", "Service discovery failed")
                 gatt?.disconnect()
                 return
             }
+            scope.launch {
+                gatt!!.services.forEach { gattService ->
+                    Log.d(
+                        "BEL",
+                        "Discovered ${gattService.uuid} service:"
+                    )
+                    if (gattService.uuid.toString() == "0000ffe0-0000-1000-8000-00805f9b34fb") {
+                        gattService.characteristics.forEach { characteristic ->
+                            Log.d(
+                                "BEL",
+                                "characteristic ${characteristic.uuid} read ${characteristic.supportsReading()} : notify or indicate ${characteristic.supportsNotifyOrIndicate()} "
+                            )
+                            Log.d("BEL", "has properties ${characteristic.properties}")
+                            if (characteristic.supportsNotify()) {
+                                Log.d("BEL", "try notify to ${characteristic.uuid}")
+                                gatt.setCharacteristicNotification(
+                                    characteristic,
+                                    true
+                                )
+                                delay(450L)
+                            }
+                            if (characteristic.supportsReading()) {
+                                gatt.readCharacteristic(characteristic)
+                                delay(450L)
+                            }
+                            if (characteristic.descriptors.size > 0) {
+                                characteristic.descriptors.forEach { descriptor ->
+                                    if (descriptor.uuid.toString() == "00002902-0000-1000-8000-00805f9b34fb") {
+                                        descriptor.setValue(byteArrayOf(1, 1))
+                                        Log.d("BEL", "write to descriptor ${descriptor.uuid}")
+                                        delay(300L)
+                                    }
+
+                                    val res = gatt.readDescriptor(descriptor)
+                                    Log.d("BEL", "descr uuid ${descriptor.uuid} try to read $res")
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
         }
 
-        override fun onCharacteristicRead(
-            gatt: BluetoothGatt,
-            characteristic: BluetoothGattCharacteristic,
-            value: ByteArray,
+        override fun onDescriptorRead(
+            gatt: BluetoothGatt?,
+            descriptor: BluetoothGattDescriptor?,
             status: Int
         ) {
-            super.onCharacteristicRead(gatt, characteristic, value, status)
-            Log.d("BEL", "got Read $characteristic with value $value")
+            Log.d(
+                "BEL",
+                "onDescriptorDeprRead ${descriptor!!.uuid} with value ${descriptor.value.asList()}"
+            )
+            super.onDescriptorRead(gatt, descriptor, status)
+        }
+
+        override fun onDescriptorWrite(
+            gatt: BluetoothGatt?,
+            descriptor: BluetoothGattDescriptor?,
+            status: Int
+        ) {
+            Log.d(
+                "BEL",
+                "descriptor ${descriptor?.uuid} writen with value ${descriptor?.value?.asList()}"
+            )
+            super.onDescriptorWrite(gatt, descriptor, status)
+        }
+
+        override fun onCharacteristicChanged(
+            gatt: BluetoothGatt?,
+            characteristic: BluetoothGattCharacteristic?
+        ) {
+            Log.d(
+                "BEL",
+                "Depricated chracteristic changed ${characteristic?.uuid} with value ${characteristic?.value?.asList()}"
+            )
         }
 
         override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
@@ -183,25 +256,30 @@ class BLEssedCentral(private val context: Activity, private val scope: Coroutine
                         bleHandler?.postDelayed(discoverServicesRunnable!!, delay.toLong())
                     } else if (bondstate == BOND_BONDING) {
                         // Bonding в процессе, ждем когда закончится
-                        Log.i("BEL", "waiting for bonding to complete")
+                        Log.d("BEL", "waiting for bonding to complete")
                     }
                 } else {
+                    Log.d("BEL", "! STATE_CONNECTED")
                     gatt.close()
                 }
             } else {
                 // Произошла ошибка... разбираемся, что случилось!
+                Log.d("BEL", "Произошла ошибка... разбираемся, что случилось!")
+                gatt.close()
+                gatt.disconnect()
 
-                gatt.close();
+
+                gatt.connect()
             }
         }
     }
 
     suspend fun connectTo(device: BluetoothDevice): Int {
+
         Log.d("BEL", "connecting to ${device.name}")
         controllerDevice = device
         val gatt: BluetoothGatt =
             controllerDevice!!.connectGatt(context, false, connectionStateCallback)
         return 0
     }
-
 }
