d64e4bc2db6af1bf646e5071e6b0ca207caebb69
falconArdente <iwsokolov@ya.ru>
2024-09-18 00:14:51 +0300

    did combine log for GattProfile analyst

--- src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/BLEssedCentral.kt
+++ src/main/java/com/example/carcamerasandlightsbluetooth/data/bluetooth/BLEssedCentral.kt
@@ -11,12 +11,8 @@ import android.bluetooth.BluetoothGatt.GATT_FAILURE
 import android.bluetooth.BluetoothGatt.GATT_SUCCESS
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_SIGNED_WRITE
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE
-import android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE
-import android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
-import android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE
-import android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_SIGNED
+import android.bluetooth.BluetoothGattService
+import android.bluetooth.BluetoothGattService.SERVICE_TYPE_PRIMARY
 import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
@@ -27,24 +23,40 @@ import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import com.welie.blessed.supportsIndicate
 import com.welie.blessed.supportsNotify
-import com.welie.blessed.supportsNotifyOrIndicate
 import com.welie.blessed.supportsReading
-import kotlinx.coroutines.CoroutineScope
+import com.welie.blessed.supportsWritingWithResponse
+import com.welie.blessed.supportsWritingWithoutResponse
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import java.util.Locale
+import java.util.UUID
 
 
-class BLEssedCentral(private val context: Activity, private val scope: CoroutineScope) {
+class BLEssedCentral(
+    private val context: Activity,
+    private val serviceToFind: BluetoothGattService = BluetoothGattService(
+        UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb"),
+        SERVICE_TYPE_PRIMARY
+    )
+) {
     enum class BleStatus {
         NOT_CONNECTED, CONNECTED, CONNECTED_NOTIFICATIONS
     }
 
     private var status: BleStatus = BleStatus.NOT_CONNECTED
+
+    //        set(value) {
+//            when (value) {
+//                BleStatus.NOT_CONNECTED -> currentGattProfile = null
+//                BleStatus.CONNECTED -> TODO()
+//                BleStatus.CONNECTED_NOTIFICATIONS -> TODO()
+//            }
+//            field = value
+//        }
     private var bleHandler = Handler(Looper.getMainLooper())
     private var scanJob: Job? = null
     private var discoverServicesRunnable: Runnable? = null
@@ -52,12 +64,16 @@ class BLEssedCentral(private val context: Activity, private val scope: Coroutine
     private var adapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
     private var scanner: BluetoothLeScanner = adapter.bluetoothLeScanner
     private var currentGattProfile: BluetoothGatt? = null
-    private var currentCharacteristic: BluetoothGattCharacteristic? = null
+    private var serviceToCommunicateWith: BluetoothGattService? = null
+    private var characteristicToWriteTo: BluetoothGattCharacteristic? = null
+    private var characteristicToNotifyTo: BluetoothGattCharacteristic? = null
     private val scanSettings =
         ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
             .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
             .setMatchMode(ScanSettings.MATCH_MODE_STICKY)
-            .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT).setReportDelay(3L).build()
+            .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
+            .setReportDelay(3L)
+            .build()
 
     fun getStatus() = status
     suspend fun startRawScan(): Flow<List<ScanResult>> = callbackFlow {
@@ -124,29 +140,33 @@ class BLEssedCentral(private val context: Activity, private val scope: Coroutine
             override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
                 super.onServicesDiscovered(gatt, status)
                 if (status == GATT_FAILURE) {
-                    Log.d("BEL", "Service discovery failed")
+                    Log.d("BLEssedScann", "Service discovery failed")
                     gatt?.disconnect()
                 }
-                gatt!!.services.forEach { gattService ->
-                    Log.d(
-                        "BEL", "Discovered ${gattService.uuid} service:"
-                    )
-                    if (gattService.uuid.toString() == "0000ffe0-0000-1000-8000-00805f9b34fb") {
-                        gattService.characteristics.forEach { characteristic ->
-                            Log.d(
-                                "BEL",
-                                "characteristic ${characteristic.uuid} read ${characteristic.supportsReading()} : notify or indicate ${characteristic.supportsNotifyOrIndicate()} "
-                            )
-                            Log.d("BEL", "has properties ${characteristic.properties}")
-                            if (characteristic.supportsNotify()) {
-                                Log.d("BEL", "try notify to ${characteristic.uuid}")
-                                gatt.setCharacteristicNotification(
-                                    characteristic, true
-                                )
-                            }
-                        }
-                    }
-                }
+                Log.d("BLEssedScann", showGattContents(gatt!!))
+//                gatt!!.services.forEach { gattService ->
+//                    Log.d(
+//                        "BEL", "Discovered ${gattService.uuid} service:"
+//                    )
+//                    if (gattService.uuid.toString() == "0000ffe0-0000-1000-8000-00805f9b34fb") {
+//                        gattService.characteristics.forEach { characteristic ->
+//                            Log.d(
+//                                "BEL",
+//                                "characteristic ${characteristic.uuid} read ${characteristic.supportsReading()} : notify or indicate ${characteristic.supportsNotifyOrIndicate()} "
+//                            )
+//                            Log.d(
+//                                "BEL",
+//                                "write with Response ${characteristic.supportsWritingWithResponse()} write no Response ${characteristic.supportsWritingWithoutResponse()}"
+//                            )
+//                            if (characteristic.supportsNotify()) {
+//                                Log.d("BEL", "try notify to ${characteristic.uuid}")
+//                                gatt.setCharacteristicNotification(
+//                                    characteristic, true
+//                                )
+//                            }
+//                        }
+//                    }
+//                }
 
             }
 
@@ -159,7 +179,11 @@ class BLEssedCentral(private val context: Activity, private val scope: Coroutine
                 }
             }
 
-            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            override fun onConnectionStateChange(
+                gatt: BluetoothGatt,
+                status: Int,
+                newState: Int
+            ) {
                 if (status == GATT_SUCCESS) {
                     if (newState == BluetoothProfile.STATE_CONNECTED) {
                         this@BLEssedCentral.status = BleStatus.CONNECTED
@@ -214,6 +238,22 @@ class BLEssedCentral(private val context: Activity, private val scope: Coroutine
         if (status == BleStatus.NOT_CONNECTED) return
 
 
+    }
 
+    fun showGattContents(profile: BluetoothGatt): String {
+        var outLog = ""
+        profile.services.forEach { gattService ->
+            outLog += "Discovered ${gattService.uuid} service:\n"
+            gattService.characteristics.forEach { characteristic ->
+                outLog += " Characteristic ${characteristic.uuid}\n  "
+                if (characteristic.supportsReading()) outLog += "read |"
+                if (characteristic.supportsWritingWithResponse()) outLog += "write with response |"
+                if (characteristic.supportsWritingWithoutResponse()) outLog += "write NO response |"
+                if (characteristic.supportsIndicate()) outLog += "indicate |"
+                if (characteristic.supportsNotify()) outLog += "notify |"
+                outLog += " \n"
+            }
+        }
+        return outLog
     }
 }
